a80d6f3f8521b51b29589de3491e8f6c946fa981
unknown <erdem.bakir@tedu.edu.tr>
2026-05-13 23:47:18 +0200

    first commit

---
+++ src/data_collection.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import openpyxl
+import openai
+import google.genai as genai
+import anthropic
+import huggingface_hub
+from dotenv import load_dotenv
+import os
+import requests
+from model_call import get_openai_response, get_claude_response, get_qwen_response, get_llama_response
+
+
+#Reading data
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "..", "data", "prompts.xlsx")
+df = pd.read_excel(file_path)
+
+models = ["gpt", "claude", "qwen", "llama"]
+results = []
+
+for index, row in df.iterrows():
+    prompt_id = row["id_prompt"]
+    category = row["category"]
+    text = row["text"]
+    print(index)
+    for model in models:
+        if model =="gpt":
+            response = get_openai_response(text)
+        elif model =="claude":
+            response = get_claude_response(text)
+        elif model=="qwen":
+            response = get_qwen_response(text)
+        elif model=="llama":
+            response = get_llama_response(text)
+
+        
+        results.append({
+            "prompt_id": prompt_id,
+            "category": category,
+            "prompt_text": text,
+            "model": model,
+            "response": response
+        })
+
+df_results = pd.DataFrame(results)
+output_path = os.path.join(BASE_DIR, "..", "data", "responses.xlsx")
+df_results.to_excel(output_path, index=False)
---
+++ src/model_call.py
@@ -0,0 +1,101 @@
+from dotenv import load_dotenv
+import openai
+import anthropic
+import google.genai as genai
+import os
+import requests
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+hf_api_key = os.getenv("HF_API_KEY")
+
+
+client_openai = openai.OpenAI(api_key=openai_api_key)
+
+def get_openai_response(prompt_text):
+    response = client_openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt_text}
+        ],
+        temperature=0.7,
+        max_tokens=300
+    )
+    return response.choices[0].message.content
+
+
+client_anthropic = anthropic.Anthropic(api_key=anthropic_api_key)
+
+def get_claude_response(prompt_text):
+    response = client_anthropic.messages.create(
+        model="claude-haiku-4-5-20251001",
+        max_tokens=300,
+        temperature=0.7,
+        system="You are a helpful assistant. Do not use markdown formatting, headers, or bullet points in your responses.",
+        messages=[
+            {"role": "user", "content": prompt_text}
+        ]
+    )
+    return response.content[0].text
+
+client_gemini = genai.Client(api_key=gemini_api_key)
+
+def get_gemini_response(prompt_text):
+    response = client_gemini.models.generate_content(
+        model="gemini-2.0-flash-lite",
+        contents=prompt_text,
+        config=genai.types.GenerateContentConfig(
+            system_instruction="You are a helpful assistant.",
+            temperature=0.7,
+            max_output_tokens=300
+        )
+    )
+    return response.text
+
+
+API_URL_QWEN = "https://router.huggingface.co/v1/chat/completions"
+
+def get_qwen_response(prompt_text):
+    headers = {
+        "Authorization": f"Bearer {hf_api_key}"
+    }
+    payload = {
+        "model": "Qwen/Qwen2.5-7B-Instruct:together",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt_text}
+        ],
+        "temperature": 0.7,
+        "max_tokens": 300
+    }
+    response = requests.post(API_URL_QWEN, headers=headers, json=payload)
+    return response.json()["choices"][0]["message"]["content"]
+
+
+
+
+
+API_URL_LLAMA = "https://router.huggingface.co/v1/chat/completions"
+
+def get_llama_response(prompt_text):
+    headers = {
+        "Authorization": f"Bearer {hf_api_key}"
+    }
+    payload = {
+        "model": "meta-llama/Llama-3.1-8B-Instruct:novita",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt_text}
+        ],
+        "temperature": 0.7,
+        "max_tokens": 300
+    }
+    response = requests.post(API_URL_LLAMA, headers=headers, json=payload)
+    return response.json()["choices"][0]["message"]["content"]
+
+
+
+
